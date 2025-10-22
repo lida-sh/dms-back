873a1e90d14db84a497bd8087330c6a47e18d704
lida <l_shafiei_2005@yahoo.com>
2025-10-22 15:55:23 +0330

    start reverb

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -29,18 +29,16 @@ return [
     */
 
     'connections' => [
-
         'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'),
-                'useTLS' => false,
                 'host' => env('PUSHER_HOST', '127.0.0.1'),
-                'port' => env('PUSHER_PORT', 6001),
+                'port' => env('PUSHER_PORT', 8080),
                 'scheme' => env('PUSHER_SCHEME', 'http'),
+                'useTLS' => false,
             ],
         ],
 
--- routes/channels.php
+++ routes/channels.php
@@ -22,4 +22,11 @@ Broadcast::channel('ocr-results', function () {
 Broadcast::channel('test-channel', function () {
     return true;
 });
+Broadcast::channel('test-ocr-channel', function ($user) {
+    return true; // دسترسی عمومی برای تست
+});
+
+Broadcast::channel('private-ocr.{userId}', function ($user, $userId) {
+    return (int) $user->id === (int) $userId;
+});
 
